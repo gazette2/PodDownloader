fa4c7fb744b52d77f481ab98f23a889159a3795b
gazette2 <gazette2@hotmail.co.kr>
2018-02-23 20:00:24 +0900

    baseline

---
+++ PodDownloader/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using System;
+
+namespace PodDownloader
+{
+	[Activity(Label = "PodDownloader", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+		}
+
+		private void SetAlarms()
+		{
+			AlarmManager alarm = (AlarmManager)GetSystemService(Context.AlarmService);
+
+			var futureDate = DateTime.Now.Date + new TimeSpan(10, 0, 0);
+			if (DateTime.Now.Hour > 10)
+				futureDate.AddHours(24);
+
+			Intent intent = new Intent(this, typeof(BatchDownloader));
+
+			PendingIntent sender = PendingIntent.GetBroadcast(this, 0, intent, PendingIntentFlags.UpdateCurrent);
+			alarm.Set(AlarmType.RtcWakeup, futureDate.Millisecond, sender);
+		}
+	}
+}
+
