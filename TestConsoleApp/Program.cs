7f136fb3aae171986dc252041d2ed816f0842ca1
gazette2 <gazette2@hotmail.co.kr>
2018-02-26 14:11:10 +0900

    minor change

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -7,7 +7,9 @@ namespace TestConsoleApp
     {
         static void Main(string[] args)
         {
+			BatchDownloader.DownloadJungsNewsShow();
+			BatchDownloader.DownloadKimsNewsFactory();
 			BatchDownloader.DownloadKimsNewsShow();
-        }
-    }
+		}
+	}
 }
