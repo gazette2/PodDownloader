fa424b6b70e7ec3b33a4763b32aa6d29e62f9f33
gazette2 <gazette2@hotmail.co.kr>
2018-03-10 23:26:26 +0900

    xml address loader implemented

--- BatchDownloader/LoadXml.cs
+++ BatchDownloader/LoadXml.cs
@@ -8,6 +8,24 @@ namespace DownloadLibrary
 {
     public static class AddressBuilder
     {
+		private static string year = DateTime.Now.Year.ToString();
+		private static string month = DateTime.Now.Month.ToString("D2");
+		private static string day = DateTime.Now.Day.ToString("D2");
+
+		public static DateTime Date
+		{
+			get
+			{
+				return new DateTime(int.Parse(year), int.Parse(month), int.Parse(day));
+			}
+			set
+			{
+				year = value.Year.ToString();
+				month = value.Month.ToString("D2");
+				day = value.Day.ToString("D2");
+			}
+		}
+
 		public static PodAddressList Load(string path)
 		{
 			XmlSerializer xmlSerializer = new XmlSerializer(typeof(PodAddressList));
@@ -15,6 +33,12 @@ namespace DownloadLibrary
 			{
 				return xmlSerializer.Deserialize(fileStream) as PodAddressList;
 			}
+		}
+
+		public static PodAddressList Load(Stream stream)
+		{
+			XmlSerializer xmlSerializer = new XmlSerializer(typeof(PodAddressList));
+			return xmlSerializer.Deserialize(stream) as PodAddressList;
 		}
 
 		public static List<string> GetEffectiveAddresses(PodAddressList addressList)
@@ -32,10 +56,6 @@ namespace DownloadLibrary
 
 		private static string Replace(string template, int index)
 		{
-			string year = DateTime.Now.Year.ToString();
-			string month = DateTime.Now.Month.ToString("D2");
-			string day = DateTime.Now.Day.ToString("D2");
-
 			StringBuilder temp = new StringBuilder(template);
 			temp.Replace("{Y}", year);
 			temp.Replace("{y}", year.Substring(2));
